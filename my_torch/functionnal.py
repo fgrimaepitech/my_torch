feat: fixing max functionnal and derivative

my_torch/functionnal.py
@@ -1,8 +1,9 @@
+from ctypes import Union
 from my_torch.tensor import Tensor
 import numpy as np
 
 from my_torch.function import Function
 import my_torch.derivatives as dv
 
-def max(tensor : Tensor) -> Tensor:
-    return Tensor(np.maximum(0, tensor.data), requires_grad=tensor.requires_grad, grad_fn=Function(dv.max_backward, [tensor]))
+def max(tensor : Tensor, floor: Union[int, float] = 0) -> Tensor:
+    return Tensor(np.maximum(floor, tensor.data), requires_grad=tensor.requires_grad, grad_fn=Function(dv.max_backward, [tensor, floor]))

my_torch/layers/relu.py
@@ -1,14 +1,16 @@
+from ctypes import Union
 from my_torch.neural_network import Module
 import numpy as np
 
 from my_torch.tensor import Tensor
+from my_torch.functionnal import max
 
 class ReLU(Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, x: Tensor) -> Tensor:
-        return Tensor(np.maximum(0, x.data), requires_grad=x.requires_grad)
+        return max(x)
 
     def extra_repr(self):
         return ""

src/analyzer.py
@@ -11,14 +11,12 @@ import numpy as np
 import torch
 
 def execute_train(args):
-    m = my_torch.ReLU()
-    input = my_torch.Tensor.randn(2).requires_grad_(True)
-    output = m(input)
-    print("using my_torch: ", output)
-    m_torch = torch.nn.ReLU()
-    input_torch = torch.randn(2).requires_grad_(True)
-    output_torch = m_torch(input_torch)
-    print("using torch: ", output_torch)
+    x = torch.randn(3, 3)
+    print(x)
+    t = torch.as_strided(x, (2, 2), (1, 2))
+    print(t)
+    t = torch.as_strided(x, (2, 2), (1, 2), 1)
+    print(t)
 
 def main():
     parser = argparse.ArgumentParser(prog="my_torch_analyzer")